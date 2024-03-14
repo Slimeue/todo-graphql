677b8edf18c298c369da88fec1a1255bd1294248
Kevinroy <77309706+Slimeue@users.noreply.github.com>
2024-03-14 23:37:16 +0800

    Update

--- src/graphql/users/user.mutation.resolver.ts
+++ src/graphql/users/user.mutation.resolver.ts
@@ -2,11 +2,13 @@ import { Args, Mutation, Resolver } from '@nestjs/graphql';
 import { User } from './User.schema';
 import { CreateUserArgs } from '../dto/create-user.input';
 import { UserService } from './user.service';
+import { Public } from 'src/utils/public.decorators';
 
 @Resolver(() => User)
 export class UserMutationResolver {
   constructor(private userService: UserService) {}
 
+  @Public()
   @Mutation(() => User)
   createUser(
     @Args('createUserData', { type: () => CreateUserArgs })
